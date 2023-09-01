c488badbd10b5ee87b7377370940da4914d651f8
Hassan Sajjad <sajjadhassa389@gmail.com>
2023-09-02 02:17:08 +0500

    feature/Initial_Backend_Changes

--- src/models/Admission.ts
+++ src/models/Admission.ts
@@ -2,8 +2,7 @@ import "reflect-metadata";
 import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
-class Immigration extends BaseEntity {
-  
+class Admission extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
@@ -11,24 +10,24 @@ class Immigration extends BaseEntity {
   name: string;
 
   @Column()
-  age: string;
-
-  @Column()
   contact: string;
 
   @Column()
-  cost: string;
-
-  @Column()
-  experience : string;
-  
-  @Column()
-  ieltsScore: string;
-
-  @Column()
-  ieltsTaken: string;
-
-  @Column()
   qualification: string;
-  }
-export default Immigration;
+
+  @Column()
+  level: string;
+
+  @Column()
+  time: string;
+
+  @Column()
+  country: string;
+
+  @Column()
+  budget: string;
+
+  @Column()
+  interest: string;
+}
+export default Admission;
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,13 +1,15 @@
 import express, {Express, Request, Response} from 'express';
 
 import immigrationRoutes from './immigrationRoutes';
+import admissionRoutes from './admissionRoutes';
 
 const userRouter: Express = express();
 
 // userRouter.get('/',AuthenticationMiddleware.isAuthentication, UserController.getUser)
 // userRouter.post('/', UserController.register)
 // userRouter.post('/login', UserController.loginUser)
-userRouter.use('/api/submit',immigrationRoutes);
+userRouter.use('/api/immigration',immigrationRoutes);
+userRouter.use('/api/admission',admissionRoutes);
 
 
 export default userRouter;
